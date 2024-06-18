2c9ac8f3d0da26ee5523da757572433a3de1884a
futo <c0a23104ef@edu.teu.ac.jp>
2024-06-18 17:59:13 +0900

    コウカトンの回転 #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -32,6 +32,7 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
+    font = pg.font.Font(None, 80)
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
@@ -40,14 +41,15 @@ def main():
     kk_rct.center = 900, 400
 
     bb_img = pg.Surface((20, 20))
-    bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
-    kokaton = kk_img
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -55,29 +57,35 @@ def main():
         if kk_rct.colliderect(bb_rct):#衝突判定
             return #ゲームオーバー
         screen.blit(bg_img, [0, 0])
+        txt = font.render(str(tmr/50), True, (255, 255, 255))
+        screen.blit(txt, [300, 200])
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+
         for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         
         if sum_mv==[-5,0]:
-            kokaton = kk_img
+            kk_img = pg.transform.rotozoom(kk_img,0,1)
         elif sum_mv==[-5,-5]:
-            kokaton = pg.transform.rotozoom(kk_img,-45,1)
+            kk_img = pg.transform.rotozoom(kk_img,-45,1)
         elif sum_mv==[-5, +5]:
-            kokaton = pg.transform.rotozoom(kk_img,45,1)
+            kk_img = pg.transform.rotozoom(kk_img,45,1)
         elif sum_mv==[+5, 0]:
-            kokaton = kk_flip_img
+            kk_img = kk_flip_img
         elif sum_mv==[+5, -5]:
-            kokaton = pg.transform.rotozoom(kk_flip_img,45,1)
+            kk_img = pg.transform.rotozoom(kk_flip_img,45,1)
         elif sum_mv==[0, -5]:
-            kokaton = pg.transform.rotozoom(kk_flip_img,90,1)
+            kk_img = pg.transform.rotozoom(kk_flip_img,90,1)
         elif sum_mv==[+5, +5]:
-            kokaton = pg.transform.rotozoom(kk_flip_img,-45,1)
+            kk_img = pg.transform.rotozoom(kk_flip_img,-45,1)
         elif sum_mv==[0, +5]:
-            kokaton = pg.transform.rotozoom(kk_flip_img,-90,1)
+            kk_img = pg.transform.rotozoom(kk_flip_img,-90,1)
+
+        kk_rct.move_ip(sum_mv)
 
 
         kk_rct.move_ip(sum_mv)
